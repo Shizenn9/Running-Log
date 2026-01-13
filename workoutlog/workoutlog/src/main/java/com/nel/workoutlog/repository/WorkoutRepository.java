894779dd8fe4b5ba757266221ca4a24352024e45
Nel <danielaimand@gmail.com>
2026-01-13 23:24:27 +0800

    first commit

--- workoutlog/workoutlog/src/main/java/com/nel/workoutlog/repository/WorkoutRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nel.workoutlog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nel.workoutlog.entity.Workout;
-
-public interface WorkoutRepository extends JpaRepository<Workout, Long> {
-}
